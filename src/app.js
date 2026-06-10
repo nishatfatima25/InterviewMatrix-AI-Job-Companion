7656838a0ac28a7511ce6a862de84c729414e6de
Nishat Fatima <its.nishatkhan004@gmail.com>
2026-06-10 14:47:45 +0530

    Add authentication system with JWT and MongoDB

--- src/app.js
+++ src/app.js
@@ -4,4 +4,9 @@ const app = express()
 
 app.use(express.json())
 
+//it requires all the routes here
+const authRouter = require("./routes/auth.routes")
+
+//then all the routes will be used here
+app.use("/api/auth", authRouter)
 module.exports = app 
